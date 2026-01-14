7a8e0c18b5c48a5c3cae05c65d1529d01e24f2e7
Samuel Matias <136845273+SamuelMatiasSfr@users.noreply.github.com>
2026-01-14 11:42:00 -0300

    Delete Exemplo-Proxy-DAO directory

--- Exemplo-Proxy-DAO/InterfaceDaoProxy.java
+++
@@ -1,9 +0,0 @@
-public interface InterfaceDaoProxy{
-
-    public int getTamanhoArray();
-    public Pessoa getPessoa(String senha, int matricula);
-    public void atualizarPessoa(String senha, int matricula, Pessoa pessoa);
-    public void deletarPessoa(String senha, int matricula);
-    public void salvarPessoas(String senha);
-
-}
